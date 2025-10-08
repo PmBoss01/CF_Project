3ee65c69eeda5276cb5e951d7e669650f529417e
Prince <mpemboateng2580@gmail.com>
2025-10-08 09:36:46 +0000

    Add IaC for saicol-frontend to saicol_infra/dop.tf

---
+++ saicol_infra/dop.tf
@@ -0,0 +1,22 @@
+provider "azurerm" {
+  features {}
+}
+
+data "azurerm_resource_group" "rg" {
+  name = "rg-prod"
+}
+
+resource "azurerm_app_service" "app" {
+  name                = "saicol-frontend"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  app_service_plan_id = "/subscriptions/sub-123/resourceGroups/rg-prod/providers/Microsoft.Web/serverfarms/app-service"
+
+  app_settings = {
+    "DB_PASSWORD": "1234"
+}
+}
+
+output "app_service_hostname" {
+  value = azurerm_app_service.app.default_site_hostname
+}
