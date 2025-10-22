c4e844a061089333d768992177863052918edba4
Prince <mpemboateng2580@gmail.com>
2025-10-22 19:10:35 +0000

    Update IaC for frontend-service at app-infra/test.tf

--- app-infra/test.tf
+++ app-infra/test.tf
@@ -2,18 +2,34 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_app_service_plan" "plan" {
-  name                = "asp-frontend-service"
-  location            = "eastus"
-  resource_group_name = "DOP_ResourceGroup"
-  kind                = "Linux"
-  reserved            = true # Required for Linux plans
-  sku {
-    tier = "Basic"
-    size = "B1"
+data "azurerm_resource_group" "rg" {
+  name = "DOP_ResourceGroup"
+}
+
+variable "app_service_plan_id" {
+  description = "The ID of the App Service Plan to use."
+  type        = string
+}
+
+variable "container_image" {
+  description = "The container image to deploy."
+  type        = string
+  default     = "placeholder.azurecr.io/image:latest"
+}
+
+resource "azurerm_app_service" "app" {
+  name                = "frontend-service"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  app_service_plan_id = var.app_service_plan_id
+
+  app_settings = {}
+
+  site_config {
+    linux_fx_version = "DOCKER|${var.container_image}"
   }
 }
 
-output "app_service_plan_id" {
-  value = azurerm_app_service_plan.plan.id
+output "app_service_hostname" {
+  value = azurerm_app_service.app.default_site_hostname
 }
