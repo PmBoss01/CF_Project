d371293fe3dca16afdbfec445cb43b2d5e740934
Prince <mpemboateng2580@gmail.com>
2025-10-22 23:55:20 +0000

    Add IaC for test-AP-IAC02 to test-002/iac-app.tf

---
+++ test-002/iac-app.tf
@@ -0,0 +1,38 @@
+provider "azurerm" {
+  features {}
+}
+
+data "azurerm_resource_group" "rg" {
+  name = "DOP_ResourceGroup"
+}
+
+variable "app_service_plan_id" {
+  description = "The ID of the App Service Plan to use."
+  type        = string
+}
+
+variable "container_image" {
+  description = "The container image to deploy."
+  type        = string
+  default     = "placeholder.azurecr.io/image:latest"
+}
+
+resource "azurerm_app_service" "app" {
+  name                = "test-AP-IAC02"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  app_service_plan_id = var.app_service_plan_id
+
+  app_settings = {
+    "KEY": "123456",
+    "APP_KEY": "123456789"
+}
+
+  site_config {
+    linux_fx_version = "DOCKER|${var.container_image}"
+  }
+}
+
+output "app_service_hostname" {
+  value = azurerm_app_service.app.default_site_hostname
+}
