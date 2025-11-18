0689e7d500dcb5d56b6ce941e99730c804b8ee8f
Prince <mpemboateng2580@gmail.com>
2025-11-18 15:08:41 +0000

    Add IaC for test-web-01 to github-test/test.tf

---
+++ github-test/test.tf
@@ -0,0 +1,19 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_app_service_plan" "plan" {
+  name                = "asp-test-web-01"
+  location            = "eastus"
+  resource_group_name = "dop-rg"
+  kind                = "Linux"
+  reserved            = true # Required for Linux plans
+  sku {
+    tier = "Basic"
+    size = "B1"
+  }
+}
+
+output "app_service_plan_id" {
+  value = azurerm_app_service_plan.plan.id
+}
