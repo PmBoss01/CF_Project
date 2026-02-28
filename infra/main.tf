b88b3bcd06b911490e768d463c8ec95155d3d16f
Prince <mpemboateng2580@gmail.com>
2026-02-28 15:01:36 +0000

    Update IaC for frontend-terraform at infra/main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,9 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
+  use_cli = false
 }
 
 resource "azurerm_app_service_plan" "plan" {
-  name                = "asp-app-terraform"
+  name                = "asp-frontend-terraform"
   location            = "centralus"
   resource_group_name = "dopRG"
   kind                = "app"
