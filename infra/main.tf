733ff8affde15945742223a7e2554989f0599811
Prince <mpemboateng2580@gmail.com>
2026-03-07 10:14:35 +0000

    Update IaC for aks-infra at infra/main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -18,10 +18,10 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_kubernetes_cluster" "aks" {
-  name                = "aks-app"
+  name                = "aks-infra"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  dns_prefix          = "aks-app"
+  dns_prefix          = "aks-infra"
 
   default_node_pool {
     name       = "agentpool"
@@ -37,7 +37,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   }
 
   tags = {
-    environment = "dev"
+    environment = "prod"
   }
 }
 
