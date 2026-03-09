053b6481020a606bb0bc0b3aa8fd3fde3d5eee23
Prince <mpemboateng2580@gmail.com>
2026-03-09 20:46:00 +0000

    Update IaC for app-v1 at infra/main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -18,10 +18,10 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_kubernetes_cluster" "aks" {
-  name                = "project-v1"
+  name                = "app-v1"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  dns_prefix          = "project-v1"
+  dns_prefix          = "app-v1"
 
   default_node_pool {
     name       = "agentpool"
