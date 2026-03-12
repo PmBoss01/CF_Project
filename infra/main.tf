cc5db04a208af3c46afebe6b113a31ea96075640
Prince <mpemboateng2580@gmail.com>
2026-03-12 20:49:37 +0000

    Add IaC for app-infra-v1 to infra/main.tf

---
+++ infra/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_app_service_plan" "plan" {
+  name                = "asp-app-infra-v1"
+  location            = "centralus"
+  resource_group_name = "dopRG"
+  kind                = "app"
+  reserved            = true
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+output "app_service_plan_id" {
+  value = azurerm_app_service_plan.plan.id
+}
