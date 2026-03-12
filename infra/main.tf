add715937a2ae4bbe683002b888accbe3589eec3
Prince <mpemboateng2580@gmail.com>
2026-03-12 13:16:01 +0000

    Update IaC for asap-app-v1 at infra/main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -12,7 +12,7 @@ provider "azurerm" {
 }
 
 resource "azurerm_app_service_plan" "plan" {
-  name                = "asp-asap-v1"
+  name                = "asp-asap-app-v1"
   location            = "centralus"
   resource_group_name = "dopRG"
   kind                = "app"
