eefe81aef4f99f98da74b422fbff6ae83cbd6e1f
Prince <mpemboateng2580@gmail.com>
2026-03-12 22:03:04 +0000

    Update IaC for aks-vv1 at infra/main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -2,27 +2,55 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.0"
+      version = "~> 3.0"
     }
   }
 }
 
 provider "azurerm" {
   features {}
+  skip_provider_registration = false
 }
 
-resource "azurerm_app_service_plan" "plan" {
-  name                = "asp-canary-infra"
-  location            = "centralus"
-  resource_group_name = "dopRG"
-  kind                = "app"
-  reserved            = true
-  sku {
-    tier = "Standard"
-    size = "S1"
+resource "azurerm_resource_group" "rg" {
+  name     = "dopRG"
+  location = "centralus"
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "aks-vv1"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "aks-vv1"
+
+  default_node_pool {
+    name       = "agentpool"
+    node_count = 2
+    vm_size    = "Standard_DS2_v2"
+    upgrade_settings {
+      max_surge       = "1"
+      max_unavailable = "0"
+    }
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    environment = "dev"
   }
 }
 
-output "app_service_plan_id" {
-  value = azurerm_app_service_plan.plan.id
+output "kube_config" {
+  value     = azurerm_kubernetes_cluster.aks.kube_config_raw
+  sensitive = true
+}
+
+output "cluster_name" {
+  value = azurerm_kubernetes_cluster.aks.name
+}
+
+output "cluster_fqdn" {
+  value = azurerm_kubernetes_cluster.aks.fqdn
 }
