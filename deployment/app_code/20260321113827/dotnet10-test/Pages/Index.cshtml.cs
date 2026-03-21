610d1d6968cf6cb915989b8b8ae8cd4c3a8c5bfa
Prince <mpemboateng2580@gmail.com>
2026-03-21 11:45:15 +0000

    feat: Add application code for dotnet10-test

---
+++ deployment/app_code/20260321113827/dotnet10-test/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace dotnet10_test.Pages;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
