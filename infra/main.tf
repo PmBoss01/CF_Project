1c556f7d39385ed7ac8df8cb4e15ad394b32fe3e
Prince <mpemboateng2580@gmail.com>
2026-03-29 04:28:43 +0000

    Add IaC for apps-github-vv1 to infra/main.tf

---
+++ infra/main.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+data "azurerm_resource_group" "rg" {
+  name = "dopRG"
+}
+
+variable "app_service_plan_id" {
+  description = "The ID of the App Service Plan to use."
+  type        = string
+}
+
+resource "azurerm_app_service" "app" {
+  name                = "apps-github-vv1"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  app_service_plan_id = var.app_service_plan_id
+
+  app_settings = {
+    "WEBSITES_PORT": "8080"
+}
+
+  site_config {
+    linux_fx_version = "NODE|20-lts"
+    app_command_line = "echo ok > /home/site/wwwroot/robots933456.txt; test -f /home/npm-global/bin/serve || npm install -g serve --prefix /home/npm-global --quiet; /home/npm-global/bin/serve -s . -l tcp://0.0.0.0:8080"
+  }
+}
+
+output "app_service_hostname" {
+  value = azurerm_app_service.app.default_site_hostname
+}
