9697f2d789ff64da184ece1258b350630959a6ad
Prince <mpemboateng2580@gmail.com>
2026-03-30 15:10:13 +0000

    Update IaC for aks-acr-vv2 at infra/main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -18,10 +18,10 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_kubernetes_cluster" "aks" {
-  name                = "aks-acr-vv1"
+  name                = "aks-acr-vv2"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  dns_prefix          = "aks-acr-vv1"
+  dns_prefix          = "aks-acr-vv2"
 
   default_node_pool {
     name       = "agentpool"
