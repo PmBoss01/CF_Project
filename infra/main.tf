cc8345a55039d0436c00c8c0be40acdff37d75ed
Prince <mpemboateng2580@gmail.com>
2026-04-15 22:34:16 +0000

    Update IaC for test-app-vv1 at infra/main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -2,37 +2,55 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.0"
+      version = "~> 3.0"
     }
   }
 }
 
 provider "azurerm" {
   features {}
+  skip_provider_registration = false
 }
 
-data "azurerm_resource_group" "rg" {
-  name = "dopRG"
+resource "azurerm_resource_group" "rg" {
+  name     = "dopRG"
+  location = "centralus"
 }
 
-variable "app_service_plan_id" {
-  description = "The ID of the App Service Plan to use."
-  type        = string
-}
-
-resource "azurerm_app_service" "app" {
+resource "azurerm_kubernetes_cluster" "aks" {
   name                = "test-app-vv1"
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
-  app_service_plan_id = var.app_service_plan_id
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "test-app-vv1"
 
-  app_settings = {}
+  default_node_pool {
+    name       = "agentpool1"
+    node_count = 2
+    vm_size    = "Standard_DS2_v2"
+    upgrade_settings {
+      max_surge       = "1"
+      max_unavailable = "0"
+    }
+  }
 
-  site_config {
-    linux_fx_version = "DOCKER|doptestCR.azurecr.io/dotnetimg:latest"
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    environment = "prod"
   }
 }
 
-output "app_service_hostname" {
-  value = azurerm_app_service.app.default_site_hostname
+output "kube_config" {
+  value     = azurerm_kubernetes_cluster.aks.kube_config_raw
+  sensitive = true
+}
+
+output "cluster_name" {
+  value = azurerm_kubernetes_cluster.aks.name
+}
+
+output "cluster_fqdn" {
+  value = azurerm_kubernetes_cluster.aks.fqdn
 }
